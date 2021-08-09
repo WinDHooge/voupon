2a58d1c917345801a6f1e26a874c57e913d4b9ed
Winnie <winniedhooge@hotmail.be>
2021-08-09 12:07:03 +0200

    start springboot

---
+++ src/main/java/be/voupon/voupon/VouponApplication.java
@@ -0,0 +1,13 @@
+package be.voupon.voupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/voupon/voupon/VouponApplicationTests.java
@@ -0,0 +1,13 @@
+package be.voupon.voupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
