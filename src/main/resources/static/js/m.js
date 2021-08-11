01c6317bcdeac8f93b6337e395ee10714ada109f
jeroennoyez <jeroennoyez@gmail.com>
2021-08-11 16:08:21 +0200

    Added HomeController & basic styling

---
+++ src/main/resources/static/js/m.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // General methods
+    //
+    var charCount = document.querySelectorAll('.charcounter');
+    M.CharacterCounter.init(charCount);
+
+    // Main Nav
+    //
+    var elemsMainNav = document.querySelectorAll('#nav-main');
+    var optionsMainNav = {
+        edge: 'left',
+        draggable: true,
+        inDuration: 250,
+        outDuration: 200,
+        onOpenStart: null,
+        onOpenEnd: null,
+        onCloseStart: null,
+        onCloseEnd: null,
+        preventScrolling: true
+    }
+    var instances = M.Sidenav.init(elemsMainNav, optionsMainNav);
+
+    // Account Nav
+    //
+    var elemsAccountNav = document.querySelectorAll('#nav-account');
+    var optionsAccountNav = {
+        edge: 'right',
+        draggable: true,
+        inDuration: 250,
+        outDuration: 200,
+        onOpenStart: null,
+        onOpenEnd: null,
+        onCloseStart: null,
+        onCloseEnd: null,
+        preventScrolling: true
+    }
+    var instances = M.Sidenav.init(elemsAccountNav, optionsAccountNav);
+
+
+});
