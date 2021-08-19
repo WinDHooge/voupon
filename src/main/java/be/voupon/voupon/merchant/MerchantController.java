d7e3f1406d09a428d09839e90db304aee95b56d9
jeroennoyez <jeroennoyez@gmail.com>
2021-08-19 18:22:18 +0200

    Added @Controller annotation to make the MerchantController work

--- src/main/java/be/voupon/voupon/merchant/MerchantController.java
+++ src/main/java/be/voupon/voupon/merchant/MerchantController.java
@@ -2,6 +2,7 @@ package be.voupon.voupon.merchant;
 
 import be.voupon.voupon.user.User;
 import be.voupon.voupon.user.UserService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 import java.security.Principal;
 
+@Controller
 public class MerchantController {
 
     @GetMapping("/account/merchant/overview")
