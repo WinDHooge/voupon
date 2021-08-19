6b55ecc20ced8c82d8bf52a3322f8592f849d25a
Winnie <winniedhooge@hotmail.be>
2021-08-19 15:52:16 +0200

    added more variables

--- src/main/java/be/voupon/voupon/merchant/Merchant.java
+++ src/main/java/be/voupon/voupon/merchant/Merchant.java
@@ -2,6 +2,7 @@ package be.voupon.voupon.merchant;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.catalina.User;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -24,19 +25,59 @@ public class Merchant {
     @Column(name = "companyName")
     private String companyName;
 
+    @NotBlank(message = "{merchant.telephone}")
+    @Size(min = 9, message = "{merchant.telephone}")
+    @Column(name = "telephone")
+    private String telephone;
+
+    @NotBlank(message = "{merchant.email}")
+    @Email(message = "{merchant.email}")
+    @Column(name = "email")
+    private String email;
+
     @NotBlank(message = "{merchant.pageHandle}")
     @Size(min = 2, message = "{merchant.pageHandle}")
     @Pattern(regexp  = "^[A-Za-z0-9]*$")
     private String pageHandle;
 
-    @NotBlank(message = "{merchant.country}")
-    @Column(name = "country")
-    private String country;
+    @NotBlank(message = "{merchant.street}")
+    @Size(min = 5, message = "{merchant.street}")
+    @Column(name = "street")
+    private String street;
+
+    @NotBlank(message = "{merchant.number}")
+    @Size(min = 1, message = "{merchant.number}")
+    @Column(name = "number")
+    private String number;
 
     @NotBlank(message = "{merchant.postalCode}")
     @Size(min = 4, message = "{merchant.postalCode}")
     @Column(name = "postalCode")
     private String postalCode;
+
+    @NotBlank(message = "{merchant.city}")
+    @Size(min = 2, message = "{merchant.city}")
+    @Column(name = "city")
+    private String city;
+
+    @NotBlank(message = "{merchant.country}")
+    @Column(name = "country")
+    private String country;
+
+    @NotBlank(message = "{merchant.VAT}")
+    @Size(min = 10, message = "{merchant.VAT}")
+    @Column(name = "VAT")
+    private String VAT;
+
+    @NotBlank(message = "{merchant.companyDescription}")
+    @Size(min = 50, message = "{merchant.companyDescription}")
+    @Column(name = "companyDescription")
+    private String companyDescription;
+
+    @NotBlank(message = "{merchant.checkoutDescription}")
+    @Size(min = 5, message = "{merchant.checkoutDescription}")
+    @Column(name = "checkoutDescription")
+    private String checkoutDescription;
 
     @NotBlank(message = "{merchant.paypalEmail}")
     @Email(message = "{merchant.paypalEmail}")
