badec144f9ef0b0c7020018787b41922902c76b3
jeroennoyez <jeroennoyez@gmail.com>
2021-08-20 21:23:21 +0200

    Added very basic welcome html email after signup

--- src/main/java/be/voupon/voupon/user/UserController.java
+++ src/main/java/be/voupon/voupon/user/UserController.java
@@ -1,5 +1,6 @@
 package be.voupon.voupon.user;
 
+import be.voupon.voupon.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,19 +10,28 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserController {
 
     private UserService userService;
+    private EmailService emailService;
 
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Autowired
+    public void setEmailService(EmailService emailService) {
+        this.emailService = emailService;
     }
 
     public void authWithHttpServletRequest(HttpServletRequest request, String username, String password) {
@@ -40,7 +50,8 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String processForm(@Valid @ModelAttribute User user, BindingResult bindingResult, HttpServletRequest request) {
+    public String processForm(@Valid @ModelAttribute User user, BindingResult bindingResult, HttpServletRequest request)
+            throws MessagingException {
         if (bindingResult.hasErrors()) {
             return "signup";
         }
@@ -48,6 +59,20 @@ public class UserController {
         try {
             userService.save(user);
             authWithHttpServletRequest(request, user.getEmail(), user.getCheckPassWord());
+
+            // Send welcome email
+            Map<String, Object> templateModel = new HashMap<>();
+            templateModel.put("firstname", user.getFirstName());
+            templateModel.put("lastname", user.getLastName());
+            templateModel.put("email", user.getEmail());
+            templateModel.put("vouponlogo", "vouponlogo");
+            templateModel.put("emailtemplate", "email/signup.html");
+
+            emailService.sendMessageUsingThymeleafTemplate(
+                    user.getEmail(),
+                    "Welcome to Voupon",
+                    templateModel);
+
         } catch (UserService.PasswordException e) {
             bindingResult.rejectValue("passWord","user.password",e.getMessage());
             return "signup";
