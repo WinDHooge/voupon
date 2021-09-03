bb43eb0e5c7d0f970ac1587dd1522b177421dc7f
Winnie <winniedhooge@hotmail.be>
2021-09-03 14:51:25 +0200

    Customer object

--- src/main/java/be/voupon/voupon/customer/Customer.java
+++ src/main/java/be/voupon/voupon/customer/Customer.java
@@ -1,4 +1,35 @@
 package be.voupon.voupon.customer;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@Table(name= "customers")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "firstName")
+    @NotBlank(message = "{customer.firstName}")
+    @Size(max = 50, message= "{customer.firstName.size}")
+    private String firstName;
+
+    @NotBlank(message= "{customer.lastName}")
+    @Size(max = 50, message="{customer.lastName.size}")
+    @Column(name = "lastName")
+    private String lastName;
+
+    @NotBlank(message = "{customer.email.empty}")
+    @Email(message = "{customer.email.format}")
+    @Column(name = "email")
+    private String email;
 }
