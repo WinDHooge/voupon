5b121aa9a94d679e4f2a79099599583cba733c8e
Winnie <winniedhooge@hotmail.be>
2021-09-06 14:43:10 +0200

    countries in Merchant class

--- src/main/java/be/voupon/voupon/merchant/Merchant.java
+++ src/main/java/be/voupon/voupon/merchant/Merchant.java
@@ -7,10 +7,7 @@ import lombok.Setter;
 import be.voupon.voupon.user.User;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 import java.util.Set;
 
@@ -19,6 +16,32 @@ import java.util.Set;
 @Setter
 @Table(name = "merchants")
 public class Merchant {
+
+    public enum Country {
+        ALBANIE("Albanië"), ANDORRA("Andorra"), ARMENIE("Armenië"), AZERBEIDZJAN("Azerbeidzjan"), BELGIE("België"),
+        BOSNIE_EN_HERZEGOVINA("Bosnië en Herzegovina"), BULGARIJE("Bulgarije"), CYPRUS("Cyprus"), DENEMARKEN("Denemarken"),
+        DUITSLAND("Duitsland"), ESTLAND("Estland"), FINLAND("Finland"), FRANKRIJK("Frankrijk"), GEORGIE("Georgië"),
+        GRIEKENLAND("Griekenland"), HONGARIJE("Hongarije"), IERLAND("Ierland"), IJSLAND("IJsland"), ITALIE("Italië"),
+        KAZACHSTAN("Kazachstan"), KOSOVO("Kosovo"), KROATIE("Kroatië"), LETLAND("Letland"), LIECHTENSTEIN("Liechtenstein"),
+        LITHOUWEN("Lithouwen"), LUXEMBURG("Luxemburg"), MALTA("Malta"), MOLDAVIE("Moldavië"),
+        MONACO("Monaco"), MONTENEGRO("Montenegro"), NEDERLAND("Nederland"), NOORD_MACEDONIE("Noord-Macedonië"),
+        NOORWEGEN("Noorwegen"), OEKRAINE("Oekraïne"), OOSTENRIJK("Oostenrijk"), POLEN("Polen"), PORTUGAL("Portugal"),
+        ROEMENIE("Roemenië"), RUSLAND("Rusland"), SANMARINO("San Marino"), SERVIE("Servië"), SLOVANIE("Slovanië"),
+        SLOWAKIJE("Slowakije"), SPANJE("Spanje"), TSJECHIÊ("Tsjechië"), TURKIJE("Turkije"), VATICAANSTAD("Vaticaanstad"),
+        VERENIGD_KONINKRIJK("Verenigd Koninkrijk"), WIT_RUSLAND("Wit-Rusland"), ZWEDEN("Zweden"), ZWITSERLAND("Zwitserland");
+
+        public static Country EUROPE;
+
+        private final String displayName;
+
+        Country(String displayName) {
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -59,10 +82,14 @@ public class Merchant {
     @NotBlank(message = "{merchant.city}")
     @Column(name = "city")
     private String city;
-
+/*
     @NotBlank(message = "{merchant.country}")
     @Column(name = "country")
     private String country;
+*/
+    @NotNull(message = "value mismatch")
+    @Column(name = "country")
+    private Country country = Country.EUROPE;
 
     @NotBlank(message = "{merchant.VAT.empty}")
     @Size(min = 10, message = "{merchant.VAT.size}")
