9912522e6f701df65870fe1b0a0e64b09549956c
jeroennoyez <jeroennoyez@gmail.com>
2021-09-07 19:32:02 +0200

    Temp commit

--- src/main/java/be/voupon/voupon/checkout/CheckoutController.java
+++ src/main/java/be/voupon/voupon/checkout/CheckoutController.java
@@ -1,7 +1,9 @@
 package be.voupon.voupon.checkout;
 
+import be.voupon.voupon.customer.Customer;
 import be.voupon.voupon.merchant.Merchant;
 import be.voupon.voupon.merchant.MerchantService;
+import be.voupon.voupon.recipient.Recipient;
 import be.voupon.voupon.user.User;
 import be.voupon.voupon.voupon.Voupon;
 import be.voupon.voupon.voupon.VouponService;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
 
@@ -49,7 +52,48 @@ public class CheckoutController {
 
     @GetMapping("/{pageHandle:^(?!merchant$).*}/checkout/orderdetails")
     public String showCheckoutOrderDetailsStep(Model model){
-        model.addAttribute(checkoutDto);
+        model.addAttribute(checkoutDto); // @Todo:necessary?
+
+        // Create new Recipient & add to model
+        if(checkoutDto.getRecipient() == null){
+            Recipient newRecipient = new Recipient();
+            model.addAttribute("recipient",newRecipient);
+        }else{
+            model.addAttribute("recipient",checkoutDto.getRecipient());
+        }
+
+        // Create new Customer & add to model
+        if(checkoutDto.getCustomer() == null){
+            Customer newCustomer = new Customer();
+            model.addAttribute("customer",newCustomer);
+        }else{
+            model.addAttribute("customer",checkoutDto.getCustomer());
+        }
+
+
         return "checkout/orderdetails";
     }
+
+    @PostMapping(value = "/{pageHandle:^(?!merchant$).*}/checkout/orderdetails", params ="previous")
+    public String backToStepOne(){
+        return "redirect:/" + checkoutDto.getMerchant().getPageHandle();
+    }
+
+    @PostMapping(value = "/{pageHandle:^(?!merchant$).*}/checkout/orderdetails", params ="next")
+    public String updateOrderDetails(@Valid @ModelAttribute Recipient recipient, @Valid @ModelAttribute Customer cutomer){
+        //--model.addAttribute("checkoutDto", checkoutDto);
+        //--System.out.println(checkoutDto.getVoupon());
+
+        // Validate & retrieve the form ModelAttributes
+        // Binding results?
+        // Add them to the proxy checkoutDto object
+
+        return "redirect:/" + checkoutDto.getMerchant().getPageHandle() + "/checkout/ordersummary";
+    }
+
+    // Create the GetMapping for "/{pageHandle:^(?!merchant$).*}/checkout/ordersummary"
+    // Add the checkoutDto to the Model
+    // Create new Order object & add to model
+    // Create new OrderDetail object & add to model
+
 }
