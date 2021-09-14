ec7461ecd0a2342bd14c8eb1fd824622a7d5fbd4
jeroennoyez <jeroennoyez@gmail.com>
2021-09-14 22:21:41 +0200

    Redeem email to Recipient

--- src/main/java/be/voupon/voupon/order/OrderController.java
+++ src/main/java/be/voupon/voupon/order/OrderController.java
@@ -1,5 +1,6 @@
 package be.voupon.voupon.order;
 
+import be.voupon.voupon.email.EmailService;
 import be.voupon.voupon.merchant.Merchant;
 import be.voupon.voupon.merchant.MerchantService;
 import be.voupon.voupon.user.User;
@@ -14,14 +15,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.mail.MessagingException;
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class OrderController {
     private OrderService orderService;
     private UserService userService;
     private MerchantService merchantService;
+    private EmailService emailService;
 
     @Autowired
     public void setOrderService(OrderService orderService) {
@@ -36,6 +41,11 @@ public class OrderController {
     @Autowired
     public void setMerchantService(MerchantService merchantService) {
         this.merchantService = merchantService;
+    }
+
+    @Autowired
+    public void setEmailService(EmailService emailService) {
+        this.emailService = emailService;
     }
 
     @GetMapping("/redeem/{vouponCode:^(?!orderDetail$).*}")
@@ -68,7 +78,7 @@ public class OrderController {
     }
 
     @PostMapping("/redeem/{vouponCode:^(?!orderDetail$).*}")
-    public String redeemVoupon(@ModelAttribute OrderDetail orderDetail, Model model, Principal principal){
+    public String redeemVoupon(@ModelAttribute OrderDetail orderDetail, Model model, Principal principal) throws MessagingException {
         if(principal != null){
             User user = userService.getUserByEmail(principal.getName());
             model.addAttribute("user", user);
@@ -87,6 +97,29 @@ public class OrderController {
                     // Change redeemed status
                     updateOrderDetail.setRedeemed(true);
                     orderService.save(updateOrderDetail);
+
+                    // Send emails
+                    //
+                    // To Recipient
+                    Map<String, Object> redeemRecipientTemplateModel = new HashMap<>();
+                    redeemRecipientTemplateModel.put("orderDetail", updateOrderDetail);
+                    redeemRecipientTemplateModel.put("vouponlogo", "vouponlogo");
+                    redeemRecipientTemplateModel.put("emailtemplate", "email/redeem-recipient.html");
+                    emailService.sendMessageUsingThymeleafTemplate(
+                            updateOrderDetail.getRecipient().getRecipientEmail(),
+                            "Voupon successfully redeemed",
+                            redeemRecipientTemplateModel);
+
+                    // To Customer
+                    /*Map<String, Object> redeemCustomerTemplateModel = new HashMap<>();
+                    redeemCustomerTemplateModel.put("orderDetail", updateOrderDetail);
+                    redeemCustomerTemplateModel.put("vouponlogo", "vouponlogo");
+                    redeemCustomerTemplateModel.put("emailtemplate", "email/redeem-customer.html");
+                    emailService.sendMessageUsingThymeleafTemplate(
+                            updateOrderDetail.getOrder().getCustomer().getCustomerEmail(),
+                            "A voupon you offered, has been redeemed",
+                            redeemRecipientTemplateModel);*/
+
 
                 }
             }
